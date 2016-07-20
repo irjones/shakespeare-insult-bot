11570263aa00c81ad588d1fbee8c3e35b41e101b
Ian Reed Jones <ian.reed.jones@gmail.com>
2016-07-19 19:59:37 -0700

    Remove unused const

--- insultBot.js
+++ insultBot.js
@@ -1,8 +1,6 @@
 var http = require('http');
 var dispatcher = require('httpdispatcher');
 
-const PORT=8080;
-
 dispatcher.onGet("/", function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end(insult());
